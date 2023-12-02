c1ced377decc8f68706017c78d781895d2f09b3f
Штенгауэр Никита Дмитриевич <nshtengauer@corp.finam.ru>
2023-12-02 22:49:38 +0300

    FXAA improvements

--- modules/core/src/main/kotlin/com/huskerdev/openglfx/internal/shaders/FXAAShader.kt
+++ modules/core/src/main/kotlin/com/huskerdev/openglfx/internal/shaders/FXAAShader.kt
@@ -1,104 +1,68 @@
 package com.huskerdev.openglfx.internal.shaders
 
-internal class FXAAShader: PassthroughShader() {
-    override val vertexSource = """
+internal class FXAAShader: PassthroughShader(
+    fragmentSource = """
         #version 330 core
         
-        varying vec2 v_rgbNW;
-        varying vec2 v_rgbNE;
-        varying vec2 v_rgbSW;
-        varying vec2 v_rgbSE;
-        varying vec2 v_rgbM;
-        
-        uniform vec2 tex_size;
-        
-        layout (location = 0) in vec2 aPos;
-
-        void main() {
-            gl_Position = aPos;
-            texcoords(aPos * tex_size, tex_size, v_rgbNW, v_rgbNE, v_rgbSW, v_rgbSE, v_rgbM);
-        }
-        
-        void texcoords(vec2 fragCoord, vec2 resolution,
-        			out vec2 v_rgbNW, out vec2 v_rgbNE,
-        			out vec2 v_rgbSW, out vec2 v_rgbSE,
-        			out vec2 v_rgbM) {
-        	vec2 inverseVP = 1.0 / resolution.xy;
-        	v_rgbNW = (fragCoord + vec2(-1.0, -1.0)) * inverseVP;
-        	v_rgbNE = (fragCoord + vec2(1.0, -1.0)) * inverseVP;
-        	v_rgbSW = (fragCoord + vec2(-1.0, 1.0)) * inverseVP;
-        	v_rgbSE = (fragCoord + vec2(1.0, 1.0)) * inverseVP;
-        	v_rgbM = vec2(fragCoord * inverseVP);
-        }
-    """.trimIndent()
-    override val fragmentSource = """
-        #version 330 core
-        
-        #define FXAA_REDUCE_MIN   (1.0/ 128.0)
-        #define FXAA_REDUCE_MUL   (1.0 / 8.0)
         #define FXAA_SPAN_MAX     8.0
+        #define FXAA_REDUCE_MUL   (1.0/FXAA_SPAN_MAX)
+        #define FXAA_REDUCE_MIN   (1.0/128.0)
+        #define FXAA_SUBPIX_SHIFT (1.0/4.0)
         
-        varying vec2 v_rgbNW;
-        varying vec2 v_rgbNE;
-        varying vec2 v_rgbSW;
-        varying vec2 v_rgbSE;
-        varying vec2 v_rgbM;
+        vec4 fxaa(sampler2D tex, vec2 fragCoord, vec2 texSize) {
+            vec2 rcpFrame = 1. / texSize.xy;
+            vec2 uv2 = fragCoord / texSize.xy;
+            vec4 uv = vec4(uv2, uv2 - (rcpFrame * (0.5 + FXAA_SUBPIX_SHIFT)));
         
-        uniform sampler2D tex;
-        uniform vec2 tex_size;
+            vec3 rgbNW = texture(tex, uv.zw).rgb;
+            vec3 rgbNE = texture(tex, uv.zw + vec2(1,0)*rcpFrame.xy).rgb;
+            vec3 rgbSW = texture(tex, uv.zw + vec2(0,1)*rcpFrame.xy).rgb;
+            vec3 rgbSE = texture(tex, uv.zw + vec2(1,1)*rcpFrame.xy).rgb;
+            vec3 rgbM  = texture(tex, uv.xy).rgb;
         
-        layout(location = 0) out vec4 out_color;
-        
-        void main() {
-            out_color = fxaa(tex, gl_FragCoord.xy, tex_size, v_rgbNW, v_rgbNE, v_rgbSW, v_rgbSE, v_rgbM)
-        }
-        
-        vec4 fxaa(sampler2D tex, vec2 fragCoord, vec2 resolution,
-                    vec2 v_rgbNW, vec2 v_rgbNE, 
-                    vec2 v_rgbSW, vec2 v_rgbSE, 
-                    vec2 v_rgbM) {
-            vec4 color;
-            mediump vec2 inverseVP = vec2(1.0 / resolution.x, 1.0 / resolution.y);
-            vec3 rgbNW = texture2D(tex, v_rgbNW).xyz;
-            vec3 rgbNE = texture2D(tex, v_rgbNE).xyz;
-            vec3 rgbSW = texture2D(tex, v_rgbSW).xyz;
-            vec3 rgbSE = texture2D(tex, v_rgbSE).xyz;
-            vec4 texColor = texture2D(tex, v_rgbM);
-            vec3 rgbM  = texColor.xyz;
             vec3 luma = vec3(0.299, 0.587, 0.114);
             float lumaNW = dot(rgbNW, luma);
             float lumaNE = dot(rgbNE, luma);
             float lumaSW = dot(rgbSW, luma);
             float lumaSE = dot(rgbSE, luma);
             float lumaM  = dot(rgbM,  luma);
+        
             float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
             float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
-            
-            mediump vec2 dir;
+        
+            vec2 dir;
             dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
             dir.y =  ((lumaNW + lumaSW) - (lumaNE + lumaSE));
+        
+            float dirReduce = max(
+                (lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * FXAA_REDUCE_MUL),
+                FXAA_REDUCE_MIN);
+            float rcpDirMin = 1.0/(min(abs(dir.x), abs(dir.y)) + dirReduce);
             
-            float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) *
-                                  (0.25 * FXAA_REDUCE_MUL), FXAA_REDUCE_MIN);
+            dir = min(vec2( FXAA_SPAN_MAX,  FXAA_SPAN_MAX),
+                  max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX),
+                  dir * rcpDirMin)) * rcpFrame.xy;
+        
+            vec4 rgbA = (1.0/2.0) * (
+                texture(tex, uv.xy + dir * (1.0/3.0 - 0.5)).rgba +
+                texture(tex, uv.xy + dir * (2.0/3.0 - 0.5)).rgba);
+            vec4 rgbB = rgbA * (1.0/2.0) + (1.0/4.0) * (
+                texture(tex, uv.xy + dir * (0.0/3.0 - 0.5)).rgba +
+                texture(tex, uv.xy + dir * (3.0/3.0 - 0.5)).rgba);
             
-            float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
-            dir = min(vec2(FXAA_SPAN_MAX, FXAA_SPAN_MAX),
-                      max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX),
-                      dir * rcpDirMin)) * inverseVP;
+            float lumaB = dot(rgbB.rgb, luma);
+        
+            if((lumaB < lumaMin) || (lumaB > lumaMax)) return rgbA;
             
-            vec3 rgbA = 0.5 * (
-                texture2D(tex, fragCoord * inverseVP + dir * (1.0 / 3.0 - 0.5)).xyz +
-                texture2D(tex, fragCoord * inverseVP + dir * (2.0 / 3.0 - 0.5)).xyz);
-            vec3 rgbB = rgbA * 0.5 + 0.25 * (
-                texture2D(tex, fragCoord * inverseVP + dir * -0.5).xyz +
-                texture2D(tex, fragCoord * inverseVP + dir * 0.5).xyz);
-
-            float lumaB = dot(rgbB, luma);
-            if ((lumaB < lumaMin) || (lumaB > lumaMax))
-                color = vec4(rgbA, texColor.a);
-            else
-                color = vec4(rgbB, texColor.a);
-            return color;
+            return rgbB; 
+        }
+        
+        uniform sampler2D tex;
+        uniform vec2 tex_size;
+        layout(location = 0) out vec4 out_color;
+        
+        void main() {
+	        out_color = fxaa(tex, gl_FragCoord.xy, tex_size);
         }
     """.trimIndent()
-}
+)
