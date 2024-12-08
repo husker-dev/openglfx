0d2c127260d7d67242aebd305d29d439d79663b4
Штенгауэр Никита Дмитриевич <nshtengauer@corp.finam.ru>
2024-12-08 13:47:11 +0300

    Fixed JOGL initialization on macOS #22

--- modules/jogl/kotlin/com/huskerdev/openglfx/jogl/JOGLFXExecutor.kt
+++ modules/jogl/kotlin/com/huskerdev/openglfx/jogl/JOGLFXExecutor.kt
@@ -1,5 +1,6 @@
 package com.huskerdev.openglfx.jogl
 
+import com.huskerdev.grapl.gl.GLContext
 import com.huskerdev.openglfx.GLExecutor
 import com.huskerdev.openglfx.canvas.GLCanvas
 import com.huskerdev.openglfx.canvas.events.GLDisposeEvent
@@ -11,6 +12,7 @@ import com.huskerdev.openglfx.jogl.events.JOGLInitializeEvent
 import com.huskerdev.openglfx.jogl.events.JOGLRenderEvent
 import com.huskerdev.openglfx.jogl.events.JOGLReshapeEvent
 import com.jogamp.opengl.GL3
+import com.jogamp.opengl.GLProfile
 import jogamp.opengl.GLDrawableFactoryImpl
 
 @JvmField @Suppress("unused")
@@ -20,20 +22,40 @@ class JOGLExecutor: GLExecutor() {
 
     private val gl = hashMapOf<GLCanvas, GL3>()
 
-    override fun createRenderEvent(canvas: GLCanvas, currentFps: Int, delta: Double, width: Int, height: Int, fbo: Int) =
-        JOGLRenderEvent(gl[canvas]!!, GLRenderEvent.ANY, currentFps, delta, width, height, fbo)
+    override fun createRenderEvent(canvas: GLCanvas, currentFps: Int, delta: Double, width: Int, height: Int, fbo: Int): GLRenderEvent {
+        checkInit(canvas)
+        return JOGLRenderEvent(gl[canvas]!!, GLRenderEvent.ANY, currentFps, delta, width, height, fbo)
+    }
 
-    override fun createReshapeEvent(canvas: GLCanvas, width: Int, height: Int) =
-        JOGLReshapeEvent(gl[canvas]!!, GLReshapeEvent.ANY, width, height)
+    override fun createReshapeEvent(canvas: GLCanvas, width: Int, height: Int): GLReshapeEvent {
+        checkInit(canvas)
+        return JOGLReshapeEvent(gl[canvas]!!, GLReshapeEvent.ANY, width, height)
+    }
 
     override fun createInitEvent(canvas: GLCanvas): GLInitializeEvent {
-        if(!gl.containsKey(canvas))
-            gl[canvas] = GLDrawableFactoryImpl.getFactoryImpl(com.jogamp.opengl.GLProfile.getDefault()).createExternalGLContext().gl.gL3
+        checkInit(canvas)
         return JOGLInitializeEvent(gl[canvas]!!, GLInitializeEvent.ANY)
     }
 
     override fun createDisposeEvent(canvas: GLCanvas): GLDisposeEvent {
         val gl = gl.remove(canvas)
         return JOGLDisposeEvent(gl!!, GLDisposeEvent.ANY)
+    }
+
+    private fun checkInit(canvas: GLCanvas){
+        if(!gl.containsKey(canvas)){
+            // https://github.com/husker-dev/openglfx/issues/22
+            // 'java.awt.headless' is used to prevent lock on macOS, when JOGL
+            // tries to initialize using AWT classes
+            val originalHeadless = System.getProperty("java.awt.headless")
+            val ctx = GLContext.current()
+
+            System.setProperty("java.awt.headless", "true")
+            val factory = GLDrawableFactoryImpl.getFactory(GLProfile.getDefaultDevice())
+            ctx.makeCurrent()
+            gl[canvas] = factory.createExternalGLContext().gl.gL3
+            if(originalHeadless != null)
+                System.setProperty("java.awt.headless", originalHeadless)
+        }
     }
 }
