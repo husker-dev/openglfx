cb7be8598cad36db6fcb6253e66b8ab26cae5288
nshtengauer <nshtengauer@corp.finam.ru>
2024-12-14 19:07:28 +0300

    Removed effect classes

--- modules/core/kotlin/com/huskerdev/openglfx/effects/GLShaderEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.huskerdev.openglfx.effects
-
-import javafx.scene.effect.Blend
-
-// TODO: implement
-internal class GLShaderEffect: Blend() {
-
-
-}
--- modules/core/kotlin/com/huskerdev/openglfx/effects/GLShaderEffectPeer.kt
+++
@@ -1,8 +0,0 @@
-package com.huskerdev.openglfx.effects
-
-import com.sun.scenario.effect.Crop
-
-// TODO: Implement
-internal class GLShaderEffectPeer: Crop(null) {
-
-}
